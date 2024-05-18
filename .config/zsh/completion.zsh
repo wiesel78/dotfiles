b709f46c6379479360daa1a9dd4c033e10da90d8
Philip Ullrich <wurst78@gmail.com>
2024-05-18 20:04:19 +0200

    fix: dotnet completion

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -1,3 +1,24 @@
 if [ $(command -v "kubectl") ]
     then source <(kubectl completion zsh)
 fi
+
+if [ $(command -v "docker") ]
+    then source <(docker completion zsh)
+fi
+
+_dotnet_zsh_complete()
+{
+    local completions=("$(dotnet complete "$words")")
+
+    # If the completion list is empty, just continue with filename selection
+    if [ -z "$completions" ]
+    then
+        _arguments '*::arguments: _normal'
+        return
+    fi
+
+    # This is not a variable assignment, don't remove spaces!
+    _values = "${(ps:\n:)completions}"
+}
+
+compdef _dotnet_zsh_complete dotnet
